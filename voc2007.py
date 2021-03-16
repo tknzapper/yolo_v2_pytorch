982a9e3a571be504740f4e555b33bdb8fc28b7fc
dakim <tknzapper@gmail.com>
2021-03-16 14:00:42 +0900

    vectorize output

--- voc2007.py
+++ voc2007.py
@@ -6,6 +6,7 @@ import xml.etree.ElementTree as ET
 from PIL import Image
 import torch
 from torch.utils.data import Dataset
+from utils import *
 
 
 class VOCDataset(Dataset):
@@ -20,8 +21,8 @@ class VOCDataset(Dataset):
             raise RuntimeError("Dataset not found.")
 
     def _check_exists(self):
-        # print("Image Folder : {}".format(self.img_root))
-        # print("Label Folder : {}".format(self.annot_root))
+        # print("Image Folder@~ {}".format(os.path.abspath(self.img_root)))
+        # print("Label Folder@~ {}".format(os.path.abspath(self.annot_root)))
 
         return os.path.exists(self.img_root) and os.path.exists(self.annot_root)
 
@@ -35,72 +36,88 @@ class VOCDataset(Dataset):
         img_name = img_files[img_files.index("{}.jpg".format(ann_files[idx].split(".")[0]))]
         img_file = os.path.join(self.img_root, img_name)
         image = Image.open(img_file).convert("RGB")
-        current_shape = np.asarray(image)
-        current_shape = torch.tensor(current_shape.shape)
+        # current_shape = np.asarray(image)
+        # current_shape = torch.tensor(current_shape.shape)
         image = image.resize((self.resize_factor, self.resize_factor))
         image = np.array(image) / 255.
         image = image.reshape(3, self.resize_factor, self.resize_factor)
         image = torch.from_numpy(image)
 
-        if not self.train:
+        if self.train:
             xml = open(os.path.join(self.annot_root, ann_files[idx]))
             tree = ET.parse(xml)
             root = tree.getroot()
 
             size = root.find("size")
-            width = int(size.find("width").text)
-            height = int(size.find("height").text)
-            channels = int(size.find("depth").text)
+            width = float(size.find("width").text)
+            height = float(size.find("height").text)
+            # channels = int(size.find("depth").text)
 
-            objects = root.findall("object")
-            label = []
-            for _object in objects:
-                name = _object.find("name").text
-                try:
-                    _class = classes.index(name)
-                    bndbox = _object.find("bndbox")
-                    xmin = int(bndbox.find("xmin").text) / float(width)
-                    ymin = int(bndbox.find("ymin").text) / float(height)
-                    xmax = int(bndbox.find("xmax").text) / float(width)
-                    ymax = int(bndbox.find("ymax").text) / float(height)
-                    bbox = self.transcoord((xmin, ymin, xmax, ymax))
-                    label.append((_class, bbox[0], bbox[1], bbox[2], bbox[3]))
-                except:
-                    continue
-            return image, label, current_shape
+            objs = root.findall("object")
+            num_objs = len(objs)
+
+            boxes = np.zeros((num_objs, 4), dtype=np.float16)
+            gt_classes = np.zeros((num_objs), dtype=np.int8)
+
+            for i, obj in enumerate(objs):
+                bbox = obj.find('bndbox')
+                x1 = int(bbox.find('xmin').text) / width
+                y1 = int(bbox.find('ymin').text) / height
+                x2 = int(bbox.find('xmax').text) / width
+                y2 = int(bbox.find('ymax').text) / height
+
+                cls = classes.index(obj.find('name').text)
+
+                boxes[i, :] = [x1, y1, x2, y2]
+                gt_classes[i] = cls
+
+            boxes = xxyy2xywh(boxes)
+
+            return image, boxes, gt_classes
         else:
             return image
 
-    def transcoord(self, bndbox):
-        cx = float((bndbox[0] + bndbox[2]) / 2.)
-        cy = float((bndbox[1] + bndbox[3]) / 2.)
-        w = float(bndbox[2] - bndbox[0])
-        h = float(bndbox[3] - bndbox[1])
-
-        return cx, cy, w, h
-
 
 def detection_collate(batch):
-    targets = []
     imgs = []
-    sizes = []
+    targets = []
 
+    label = torch.zeros((feature_size, feature_size, (num_classes + 5) * len(anchor_box)))
     for sample in batch:
         imgs.append(sample[0])
-        sizes.append(sample[2])
 
-        label = torch.zeros((feature_size, feature_size, (num_classes + 5) * len(anchor_box)))
-        for object in sample[1]:
-            objecness = 1
-            class_idx = object[0]
+        boxes = sample[1]
+        gt_classes = sample[2]
 
-            cx = object[1]
-            cy = object[2]
-            w = object[3]
-            h = object[4]
+        scale_factor = 1 / feature_size
+        bsize, isize = boxes[:, 0:2].size()
+        objectness = torch.zeros((bsize, 1))
+        grid_index = torch.zeros((bsize, isize))
+        offset = torch.zeros((bsize, isize))
+        for b in range(bsize):
+            for i in range(isize):
+                objectness[b] = 1
+                g_index = int(float(boxes[:, 0:2][b, i].item()) // scale_factor)
+                offs = float(boxes[b, i].item() / scale_factor) - g_index
+                grid_index[b, i] = g_index
+                offset[b, i] = offs
+        grid_arr = torch.cat([objectness, offset, boxes[:, 2:4]], dim=1)
+        # print(grid_arr)
+
+        b_size, _ = grid_index.size()
+        for b in range(b_size):
+            for anchor in range(len(anchor_box)):
+                idx = (num_classes + 5) * (anchor + 1)
+                label[int(grid_index[b][0].item()), int(grid_index[b][1].item()), idx-5:idx] = grid_arr[b]
+                label[int(grid_index[b][0]), int(grid_index[b][1]), gt_classes[b]+idx-25] = 1
+
+        targets.append(label)
+
+    return torch.stack(imgs, dim=0), torch.stack(targets, dim=0)
 
 
 
 if __name__ == '__main__':
-    data = VOCDataset(data_root)
+
+    data = VOCDataset(data_root, train=True)
     print(data[0])
