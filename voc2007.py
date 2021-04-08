e6d01ddca9e18ebc9c5d6fb0db2fdb8addcc166d
dakim <tknzapper@gmail.com>
2021-04-08 16:12:05 +0900

    image augmentation with albumentation library

--- voc2007.py
+++ voc2007.py
@@ -1,4 +1,3 @@
-from config import *
 import os
 import numpy as np
 import random
@@ -6,13 +5,16 @@ import xml.etree.ElementTree as ET
 from PIL import Image
 import torch
 from torch.utils.data import Dataset
-from utils import *
+import utils
+import config as cfg
+import albumentations as A
+import cv2
 
 
 class VOCDataset(Dataset):
-    def __init__(self, data_root, train=True, transforms=None, resize=resize):
-        self.annot_root = data_root + annot_root
-        self.img_root = data_root + img_root
+    def __init__(self, data_root, train=True, transforms=None, resize=cfg.resize):
+        self.annot_root = data_root + cfg.annot_root
+        self.img_root = data_root + cfg.img_root
         self.train = train
         self.transforms = transforms
         self.resize_factor = resize
@@ -36,51 +38,54 @@ class VOCDataset(Dataset):
         img_name = img_files[img_files.index("{}.jpg".format(ann_files[idx].split(".")[0]))]
         img_file = os.path.join(self.img_root, img_name)
         image = Image.open(img_file).convert("RGB")
-        # current_shape = np.asarray(image)
-        # current_shape = torch.tensor(current_shape.shape)
-        image = image.resize((self.resize_factor, self.resize_factor))
-        image = np.array(image) / 255.
-        image = image.reshape(3, self.resize_factor, self.resize_factor)
-        image = torch.from_numpy(image)
+        image = np.array(image)
+        # image = cv2.imread(img_file)
+        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         if self.train:
             xml = open(os.path.join(self.annot_root, ann_files[idx]))
             tree = ET.parse(xml)
             root = tree.getroot()
 
-            size = root.find("size")
-            width = float(size.find("width").text)
-            height = float(size.find("height").text)
-            # channels = int(size.find("depth").text)
-
             objs = root.findall("object")
             num_objs = len(objs)
 
-            boxes = np.zeros((num_objs, 4), dtype=np.float16)
-            gt_classes = np.zeros((num_objs), dtype=np.int8)
-
+            # gt = torch.zeros((num_objs, 5))
+            bboxes = []
+            clses = []
             for i, obj in enumerate(objs):
                 bbox = obj.find('bndbox')
-                x1 = int(bbox.find('xmin').text) / width
-                y1 = int(bbox.find('ymin').text) / height
-                x2 = int(bbox.find('xmax').text) / width
-                y2 = int(bbox.find('ymax').text) / height
+                x1 = int(bbox.find('xmin').text)
+                y1 = int(bbox.find('ymin').text)
+                x2 = int(bbox.find('xmax').text)
+                y2 = int(bbox.find('ymax').text)
 
-                cls = classes.index(obj.find('name').text)
+                # cls = classes.index(obj.find('name').text)
+                cls = obj.find('name').text
+                # gt[i, :] = torch.LongTensor([x1, y1, x2, y2, cls])
+                bboxes.append([x1, y1, x2, y2])
+                clses.append(cls)
 
-                boxes[i, :] = [x1, y1, x2, y2]
-                gt_classes[i] = cls
-
-            boxes = xxyy2xywh(torch.FloatTensor(boxes))
-
-            return image, boxes, torch.LongTensor(gt_classes)
+            if self.transforms:
+                transformed = self.transforms(image=image, bboxes=bboxes, class_labels=clses)
+                print(transformed)
+                return transformed["image"], transformed["bboxes"], transformed["class_labels"]
+            else:
+                return image, bboxes, clses
         else:
-            return image
+            if self.transforms:
+                transformed = self.transforms(image=image)
+                return transformed["image"]
+            else:
+                return image
 
 
 def detection_collate(batch):
     imgs = []
     targets = []
+
+    feature_size = cfg.feature_size
+    num_classes = cfg.num_classes
 
     for sample in batch:
 
@@ -108,6 +113,24 @@ def detection_collate(batch):
 
 
 if __name__ == '__main__':
+    from visualize import *
 
-    data = VOCDataset(data_root, train=True)
-    print(data[0])
+    transforms = A.Compose([
+        # A.RandomResizedCrop(cfg.resize, cfg.resize, scale=(0.08, 1.0), ratio=(0.75, 1.33), p=0.5),
+        # A.RandomSizedBBoxSafeCrop(cfg.resize, cfg.resize, p=1),
+        A.RandomCrop(300, 300, p=1),
+        A.Resize(cfg.resize, cfg.resize, p=1),
+        A.RandomBrightnessContrast(),
+        A.HueSaturationValue(),
+    ], bbox_params=A.BboxParams(format='pascal_voc', min_visibility=0.3, label_fields=['class_labels']))
+
+    data = VOCDataset(data_root=cfg.data_root,
+                      train=True,
+                      transforms=transforms)
+
+    data[0]
+    # for b in range(cfg.batch_size):
+    #     img = data[b][0]
+    #     bboxes = data[b][1]
+    #     clses = data[b][2]
+        # visualize(img, bboxes, clses)
